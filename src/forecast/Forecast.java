f17c84b80b3fbcab8b3061cd161603d1982e86bd
Joshua Reno <jralphur@gmail.com>
2017-11-18 16:18:02 -0800

    Added the skeleton files; first commit.

---
+++ src/forecast/Forecast.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forecast;
+
+import org.json.JSONObject;
+//1. At least 3 classes / objects NOT including your "main" class.
+//
+
+
+
+//2. You'll need to read data from a file
+//
+//3. You'll need to write data to a file
+//
+//4. You'll need to accept input from the user. 
+
+/*
+ 2 & 3 can be ForecastIO
+ Menu Class
+*/
+/**
+ *
+ * @author Joshua
+ */
+public class Forecast {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* the plan */
+       /* init relevant classes */
+       /* check to see if favorites file exists, if it doesn't, create it */
+       /* load favorites file */
+       /* load json data from favorites */
+       /* show favorite forecast information */
+       /* main menu */
+       /* 1. check weather for city */
+       /* 2. check and save city for favorites */
+       /* 3. delete cities from favorites */
+       /* 4. queue your favorites */
+       /* 5. quit */
+       
+    }
+    
+}
